88d05bc3392530e923f69a86be4b8bd41592c7be
Alexander Hoinville <ahoin001@gmail.com>
2020-03-06 22:13:21 -0500

    Able to upload image to cloudinary for persistant saving, but need to change app to get image from cloudinary url instead of local folder

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const { check } = require('express-validator')
 const fileUpload = require('../middleware/file-upload')
+const cloudUpload = require('../middleware/cloudUpload')
 const usersControllers = require('../controller/users-controller')
 
 router.get('/', usersControllers.getUsers)
@@ -10,7 +11,7 @@ router.get('/', usersControllers.getUsers)
 // .single middleware to expect a single file with the name image
 router.post(
     '/signup',
-    fileUpload.single('image'), // image is key used to find file in post request body object
+    cloudUpload.single('image'), // image is key used to find file in post request body object
     [
         check('name')
             .not()
